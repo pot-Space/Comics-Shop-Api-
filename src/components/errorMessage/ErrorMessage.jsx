81f5a0218702fab8cf220e65c30f243a100939a2
Maxim <medey4work@gmail.com>
2023-11-28 17:40:52 +0300

    add method onError, spinner

---
+++ src/components/errorMessage/ErrorMessage.jsx
@@ -0,0 +1,13 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+   return (
+      <img
+         src={img}
+         alt="Error"
+         style={{ display: "block", width: "250px", height: "250px", objectFit: "contain", margin: "0 auto" }}
+      />
+   )
+}
+
+export default ErrorMessage;
