2d3df63afadd5ff6c8f7d785ece5f40cc7f71123
Maxim <medey4work@gmail.com>
2023-12-07 14:28:48 +0300

    fix comics length and vrew img

--- src/components/charInfo/CharInfo.jsx
+++ src/components/charInfo/CharInfo.jsx
@@ -83,10 +83,16 @@ class CharInfo extends Component {
 const View = ({ char }) => {
    const { name, description, thumbnail, homepage, wiki, comics } = char;
 
+   let imgStyle = { 'objectFit': 'cover' };
+
+   if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+      imgStyle = { 'objectFit': 'contain' };
+   }
+
    return (
       <>
          <div className="char__basics">
-            <img src={thumbnail} alt={name} />
+            <img src={thumbnail} alt={name} style={imgStyle} />
             <div>
                <div className="char__info-name">{name}</div>
                <div className="char__btns">
@@ -104,18 +110,19 @@ const View = ({ char }) => {
          </div>
          <div className="char__comics">Comics:</div>
          <ul className="char__comics-list">
+            {comics.length > 0 ? null : 'There is no comics with this character'}
             {
                comics.map((item, i) => { // i - номер по порядку
-                  if (i < 10) {
-                     return (
-                        <li key={i} className="char__comics-item">
-                           {item.name}
-                        </li>
-                     )
-                  }
+                  // eslint-disable-next-line
+                  if (i > 9) return;
+
+                  return (
+                     <li key={i} className="char__comics-item">
+                        {item.name}
+                     </li>
+                  )
                })
             }
-
          </ul>
       </>
    )
